bad3d5b6a20aaf399d64b742b6744055ed053f3a
Marius Jopen <mail@mariusjopen.com>
2025-08-24 12:46:13 +0200

    about

--- src/lib/slices/RichText/index.svelte
+++ src/lib/slices/RichText/index.svelte
@@ -1,28 +1,13 @@
 <script lang="ts">
 	import type { Content } from '@prismicio/client';
 	import { PrismicRichText, type SliceComponentProps } from '@prismicio/svelte';
-	import Label from './Label.svelte';
 
 	type Props = SliceComponentProps<Content.RichTextSlice>;
 
 	const { slice }: Props = $props();
 </script>
 
-<section class="container">
+<section class="px-3 max-w-8/12 mx-auto pb-24">
 	<PrismicRichText
-		field={slice.primary.content}
-		components={{
-			label: Label
-		}}
-	/>
+		field={slice.primary.content} />
 </section>
-
-<style>
-	.container {
-		max-width: 600px;
-		margin: 6em auto;
-		font-family:
-			-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif,
-			'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
-	}
-</style>
