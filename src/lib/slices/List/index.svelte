1ccb2369ad7e538f185d556461a5a14db2562830
Marius Jopen <mail@mariusjopen.com>
2025-12-10 12:22:39 +0100

    s

--- src/lib/slices/List/index.svelte
+++ src/lib/slices/List/index.svelte
@@ -87,17 +87,17 @@
 						class="list-item block py-2.5 transition-all duration-500 ease-out {visibleItems.has(index) ? 'opacity-100 translate-y-0' : 'opacity-100 translate-y-0'} {isMobile ? '' : 'hover:text-black'}"
 					>
 						<div class="grid grid-cols-12 items-center gap-2 paragraph-1">
-							<div class="col-span-4 md:col-span-3 text-left tracking-wide text-xs md:text-2xl">{linkText || 'Magazine'}</div>
-							<div class="col-span-6 md:col-span-6 text-center text-xs md:text-2xl">{item.text || ''}</div>
-							<div class="col-span-2 md:col-span-3 text-right text-xs md:text-2xl">{formatYear(item.year)}</div>
+							<div class="col-span-6  md:col-span-3 text-left tracking-wide text-base md:text-2xl">{linkText || 'Magazine'}</div>
+							<div class="col-span-6  md:col-span-6 text-center text-base md:text-2xl">{item.text || ''}</div>
+							<div class="col-span-6  md:col-span-3 text-right text-base md:text-2xl hidden md:block">{formatYear(item.year)}</div>
 						</div>
 					</PrismicLink>
 				{:else}
 					<div class="list-item block py-2.5 transition-all duration-500 ease-out {visibleItems.has(index) ? 'opacity-100 translate-y-0' : 'opacity-100 translate-y-0'} {isMobile ? '' : 'hover:text-black'}">
 						<div class="grid grid-cols-12 items-center gap-2 paragraph-1">
-							<div class="col-span-4 md:col-span-3 text-left tracking-wide text-xs md:text-2xl">{linkText || 'Magazine'}</div>
-							<div class="col-span-6 md:col-span-6 text-center text-xs md:text-2xl">{item.text || ''}</div>
-							<div class="col-span-2 md:col-span-3 text-right text-xs md:text-2xl">{formatYear(item.year)}</div>
+							<div class="col-span-6  md:col-span-3 text-left tracking-wide text-base md:text-2xl">{linkText || 'Magazine'}</div>
+							<div class="col-span-6  md:col-span-6 text-center text-base md:text-2xl">{item.text || ''}</div>
+							<div class="col-span-6  md:col-span-3 text-right text-base md:text-2xl hidden md:block">{formatYear(item.year)}</div>
 						</div>
 					</div>
 				{/if}
