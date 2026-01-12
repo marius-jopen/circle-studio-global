64c92cc9906b560aa6fd7a340898cb117d5f58a6
Marius Jopen <mail@mariusjopen.com>
2026-01-12 17:30:10 -0300

    dd

--- src/routes/api/auth/logout/+server.ts
+++
@@ -1,8 +0,0 @@
-import { json } from '@sveltejs/kit';
-import { clearAuthCookie } from '$lib/utils/auth';
-import type { RequestHandler } from './$types';
-
-export const POST: RequestHandler = async ({ cookies }) => {
-	clearAuthCookie(cookies);
-	return json({ success: true });
-};
