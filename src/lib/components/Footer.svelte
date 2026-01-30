c2a1a535ca781559b3e1d76b0b77a3c20a3cb163
Marius Jopen <mail@mariusjopen.com>
2026-01-30 07:48:49 -0300

    footer black

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <footer class="w-full px-3 pb-22 md:pb-4 ">
-  <div class="md:bg-neutral-100 px-4 pb-3 rounded-full md:flex pt-6 md:pt-3 justify-center mt-[-1px] md:justify-between items-center w-full pt-3 relative">
+  <div class="md:bg-neutral-900 text-white px-4 pb-3 rounded-full md:flex pt-6 md:pt-3 justify-center mt-[-1px] md:justify-between items-center w-full pt-3 relative">
     <div class="mb-2 md:mb-0 text-sm text-center md:text-left">© {currentYear} ART CAMP EST.2016</div>
     {#if settings?.data?.navigation_footer?.length}
       <ul class="hidden md:flex items-center space-x-6 absolute left-1/2 -translate-x-1/2">
@@ -15,7 +15,7 @@
           <li>
             <PrismicLink
               field={navItem}
-              class="hover:text-neutral-900 transition-colors duration-300 text-sm"
+              class="hover:text-neutral-900 transition-colors duration-300 text-sm text-white"
             >
               {navItem.text || 'Link'}
             </PrismicLink>
